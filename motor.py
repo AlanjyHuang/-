404a5e5e61c70532bfbd03c020f5f0b1fbdb33fb
AlanjyHuang <52521773+AlanjyHuang@users.noreply.github.com>
2022-01-08 16:45:43 +0800

    import

--- motor.py
+++ motor.py
@@ -1,26 +1,25 @@
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+     print('RunTimeError')
+import time
 class motor:
     def __init__(self) :
-        try:
-            import RPi.GPIO as GPIO
-        except RuntimeError:
-            print('RunTimeError')
-        import time
-
-        PORT = 14
+        self.PORT = 14
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(PORT, GPIO.OUT)
+        GPIO.setup(self.PORT, GPIO.OUT)
 
     def blink(self,times):
-        GPIO.output(PORT, GPIO.LOW)  #GPIO.output(PORT, GPIO.TRUE)
+        GPIO.output(self.PORT, GPIO.LOW)  #GPIO.output(PORT, GPIO.TRUE)
         while times > 0:
             time.sleep(1)
             print(times)
             times -= 1
-        GPIO.output(PORT, GPIO.HIGH)
+        GPIO.output(self.PORT, GPIO.HIGH)
         GPIO.cleanup()
 
 
 
 if __name__ == '__main__':
     mymotor=motor()
-    mymotor.blink(20)
+    mymotor.blink(5)
