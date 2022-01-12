3442833fd8e842ec4e1621396b7de477c499f1cc
James <j212031@nehs.hc.edu.tw>
2022-01-13 01:00:21 +0800

    Update ReadWebcam.py

--- ReadWebcam.py
+++ ReadWebcam.py
@@ -1,50 +1,45 @@
 # importing required libraries
 import cv2
 import numpy as np
-import time
+import matplotlib.pyplot as plt
+
+from imutils.video import VideoStream
+from xlsxwriter import Workbook
+
 class color:
     def run(self):
 # taking the input from webcam
-        vid = cv2.VideoCapture(0)
+
+        plt.ion()  # Set interactive mode on
+
+        # We will be using Video-capture to get the fps value.
+        capture = cv2.VideoCapture(0)
+
+        capture.release()
+
+        # New module: VideoStream
+        vs = VideoStream().start()
 
         timers = 5
 
-# running while loop just to make sure that
-# our program keep running untill we stop it
         while timers>0:
+            frame = vs.read()
 
-            # capturing the current frame
-            _, frame = vid.read()
+            if frame is None:
+                print("Frame is not found!")
+                break
+            
+            b, g, r = cv2.split(frame)
 
-            # displaying the current frame
-            #cv2.imshow("frame", frame)
-
-            # setting values for base colors
-            b = frame[:, :, :1]
-            g = frame[:, :, 1:2]
-            r = frame[:, :, 2:]
-
-            # computing the mean
             b_mean = np.mean(b)
             g_mean = np.mean(g)
             r_mean = np.mean(r)
 
-
-
-            print("r:" + str(r_mean) + ",g:" + str(g_mean) + ",b:" + str(b_mean))
-
-                # displaying the most prominent color
-            if (b_mean > g_mean and b_mean > r_mean):
-                print("Blue")
-            if (g_mean > r_mean and g_mean > b_mean):
-                print("Green")
-            else:
-                print("Red")
-
-            time.sleep(1)
+            
+            print("r:"+str(r_mean)+", g:"+str(g_mean) + ", b:" + str(b_mean))
             timers -= 1
             return list([r_mean,g_mean,b_mean])
-
+        
 if __name__ =='__main__':
         mydev=color()
         print(mydev.run())
